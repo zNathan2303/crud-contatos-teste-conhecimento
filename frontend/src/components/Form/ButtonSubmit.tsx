eda5314b8f67c22a0a2817b3f840ecf44eafb45b
Nathan da Silva Costa <nathansilvacosta2303@gmail.com>
2025-12-17 14:04:05 -0300

    feat: cria botão para enviar os dados do formulário

---
+++ frontend/src/components/Form/ButtonSubmit.tsx
@@ -0,0 +1,10 @@
+export default function ButtonSubmit() {
+  return (
+    <button
+      type="submit"
+      className="bg-button w-fit text-white font-medium text-lg px-3 py-2 rounded-md hover:bg-primary transition hover:scale-submit cursor-pointer active:scale-95"
+    >
+      Cadastrar contato
+    </button>
+  );
+}
